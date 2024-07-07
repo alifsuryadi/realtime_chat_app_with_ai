0913a02503409f0cd4a2d8855963a7e128ce1681
Alif Suryadi <alifsuryadi037@gmail.com>
2024-07-07 17:32:37 +0700

    add assets & fix splash screen

--- lib/screens/overview_screen.dart
+++ lib/screens/overview_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login_screen.dart';
+// import 'login_screen.dart';
 
 class OverviewScreen extends StatelessWidget {
   const OverviewScreen({super.key});
@@ -7,16 +7,21 @@ class OverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const LoginScreen()),
-            );
-          },
-          child: const Text('Go to Login'),
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Image.asset("assets/images/icons/shape-icon.png"),
         ),
+
+        // child: ElevatedButton(
+        //   onPressed: () {
+        //     Navigator.pushReplacement(
+        //       context,
+        //       MaterialPageRoute(builder: (context) => const LoginScreen()),
+        //     );
+        //   },
+        //   child: const Text('Mendaftar dengan Email'),
+        // ),
       ),
     );
   }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,22 +1,75 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:realtime_chat_app_with_ai/screens/overview_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int _remainingSeconds = 10; // Durasi dalam detik
+
+  @override
+  void initState() {
+    super.initState();
+    _startCountdown();
+    _navigateToNextScreen();
+  }
+
+  void _startCountdown() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_remainingSeconds == 0) {
+        timer.cancel();
+      } else {
+        setState(() {
+          _remainingSeconds--;
+        });
+      }
+    });
+  }
+
+  void _navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 10), () {});
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const OverviewScreen()),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const OverviewScreen()),
-            );
-          },
-          child: const Text('Go to Login'),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Image.asset(
+                    "assets/images/amboai-green.png",
+                    width: 250,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const CircularProgressIndicator(),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Text(
+              'Loading... $_remainingSeconds seconds remaining',
+              style: const TextStyle(fontSize: 16, color: Colors.black),
+            ),
+          ),
+        ],
       ),
     );
   }
